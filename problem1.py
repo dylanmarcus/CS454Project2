1fc0313143a8e335178963f25c1d40741f438671
Dylan Marcus <dylan.marcus@icloud.com>
2018-04-04 15:56:49 -0700

    Problem1 builds NFA

--- problem1.py
+++ problem1.py
@@ -1,59 +1,25 @@
-import numpy as np
+from __future__ import print_function
 
 
-"""
-table is an NFA that rejects any int (as a string) that is not
-divisible by k, where table[i] is an NFA stateand table[j] is
-the transition on any given state. So table[i][j] determines
-what state will be reached, given a transition.
-"""
-def makeTable(k):
-	table = [[None for i in range(10)] for i in range(k)]
+def makeNFA(k):
+	NFA = [[ [] for i in range(10)] for i in range(k*2)]
 	for i in range(k):
 		for j in range(10):
-			table[i][j] = int( str(i) + str(j) ) % k
-	return table
+			NFA[i][j].append(int( str(i) + str(j) ) % k)
+			NFA[i+k][j].append(int( str(i) + str(j) ) % k)
+			
+	for i in range(k):
+		for j in range(10):
+			NFA[i][j].append(i + k)
+
+	return NFA
 
 
-# function to traverse an NFA, given a string n
-def isMultiple(n, table):
-	# state 0 is the starting and accepting state in all cases
-	state = 0
-	for digit in n:
-		state = table[state][int(digit)]
-	if state == 0:
-		return True
-	return False
+NFA = makeNFA(3)
 
-
-def isNotStrongMultiple(k, n):
-	table = makeTable(k)
-	accepted = []
-	accepted.append(isMultiple(n, table))
-	# remove each digit from n (one at a time), resulting in Nj
-	# and run Nj through the NFA
-	for digit in range(len(n)):
-		accepted.append(isMultiple(n[:digit] + n[digit+1:], table))
-	if True in accepted:
-		print("no")
-	else:
-		print("yes")
-
-	"""
-	-MORE EFFICENT VERSION BUT INSTRUCTIONS SAY TO USE A LIST OF BOOLS-
-	table = makeTable(k)
-	if isMultiple(n, table):
-		return False
-	# remove each digit from n (one at a time), resulting in Nj
-	# and run Nj through the NFA
-	for digit in range(len(n)):
-		if isMultiple(n[:digit] + n[digit+1:], table):
-			return False
-	return True
-	"""
-
-
-
-k = input("k: ")
-n = input("n: ")
-isNotStrongMultiple(k, str(n))
+# Print out NFA just so we can visualize it
+for i in range(6):
+	print(str(i) + "| ", end='')
+	for j in range(10):
+		print(" " + str(NFA[i][j]) + " ", end='')
+	print('\n')
